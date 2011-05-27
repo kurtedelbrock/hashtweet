5086b240e972e21084cdb2fda52c4da78a0f79fb
Kurt Edelbrock <kurtedelbrock@gmail.com>
2011-05-27 16:21:16 -0400

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashtweet::Application.config.secret_token = 'a685e74afe06ba311f4b6a1a5ca4328d3f474d29ba2c078630039d8efa6c9d59b585115abfb739a3cd7cb4499aef622177070d26a4fec86797c3706dd0b20e7b'
